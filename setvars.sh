8145456edda1fa50b416b421655018779e677e89
Denis L <lepchev@gmail.com>
2023-08-28 17:09:57 +0800

    Fix webapp cli

--- setvars.sh
+++ setvars.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+echo "Setting arguments for python-llama-cpp to compile with CUBLAS. You can run pip install now."
+export CMAKE_ARGS="-DLLAMA_CUBLAS=on"
+
+if [ -n "$1" ]
 then
-    echo "Provide CUDA path as an argument, e.g. /usr/local/cuda"
-    exit 1
+    export CMAKE_ARGS="-DCMAKE_CUDA_COMPILER:PATH=$1/bin/nvcc $CMAKE_ARGS"
 fi
 
-CUDA_PATH=$1
-python3 -m pip install -U pip
-
-echo "Setting arguments for python-llama-cpp to compile with CUBLAS. You can run pip install now."
-export CMAKE_ARGS="-DCMAKE_CUDA_COMPILER:PATH=$CUDA_PATH/bin/nvcc -DLLAMA_CUBLAS=on" 
 export FORCE_CMAKE=1 
-
-# echo "Installing requirements..."
-# pip install -e .
+python3 -m pip install -U pip
--- src/llmsearch/cli.py
+++ src/llmsearch/cli.py
@@ -1,4 +1,6 @@
 import click
+import os
+import streamlit.web.cli
 
 from llmsearch.chroma import VectorStoreChroma
 from llmsearch.splade import SparseEmbeddingsSplade
@@ -16,7 +18,6 @@ def index_group():
 @click.group(name="interact")
 def interact_group():
     """Commands to interact in Q&A sessiont with embedded content using LLMs"""
-
 
 @click.group
 def main_cli():
@@ -63,9 +64,32 @@ def launch_qa_with_llm(config_file: str):
     llm_bundle = get_llm_bundle(config)
     qa_with_llm(llm_bundle, config)
 
+@click.command("webapp")
+@click.option(
+    "--config-file",
+    "-c",
+    "config_file",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False),
+    help="Specifies YAML configuration file",
+)
+def launch_streamlit(config_file: str):
+    # Based on
+    # https://discuss.streamlit.io/t/running-streamlit-inside-my-own-executable-with-the-click-module/1198/4
+    # streamlit run ./src/llmsearch/webapp.py -- --config_path ./sample_templates/obsidian_conf.yaml
+    
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'webapp.py')
+    args = ["--config_path", config_file]
+    streamlit.web.cli._main_run(filename, args)
+
 
 index_group.add_command(generate_index)
 interact_group.add_command(launch_qa_with_llm)
+
+index_group.add_command(generate_index)
+interact_group.add_command(launch_qa_with_llm)
+interact_group.add_command(launch_streamlit)
 
 # add command groups to CLI root
 main_cli.add_command(index_group)
