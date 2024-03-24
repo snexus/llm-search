241506cd2f40c5c2ec8f3a3eefe430abe4ab4481
DL <you@example.com>
2024-03-24 23:16:22 +0800

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Denis L.'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.duration", "sphinx.ext.autodoc", "sphinxcontrib.autodoc_pydantic", "sphinx_markdown_builder"]
+extensions = ["sphinx.ext.duration", "sphinx.ext.autodoc", "sphinxcontrib.autodoc_pydantic" ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
